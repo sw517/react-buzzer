97b1ccc646cd231bc96836b2f0a5327f61fa348c
Sam Wood <16246185+sw517@users.noreply.github.com>
2023-12-21 21:47:56 +0000

    WIP PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,42 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react'
 
 export default function Document() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker
+          .register('/sw.js')
+          .then((res) => console.log('service worker registered'))
+          .catch((err) => console.log('service worker not registered\n', err))
+      })
+    }
+  }, [])
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="manifest.json" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <meta name="apple-mobile-web-app-status-bar" content="#2d3035" />
+        <meta name="theme-color" content="#2d3035" />
+      </Head>
       <body>
         <Main />
         <NextScript />
